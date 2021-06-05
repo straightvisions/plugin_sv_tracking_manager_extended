7298dbcc3c4ab589eb5ba8bb40f0e8163560135a
Matthias Bathke <matthias-bathke@straightvisions.com>
2021-06-05 09:55:01 +0200

    UPDATE PLAUSIBLE

--- lib/modules/plausible/plausible.php
+++ lib/modules/plausible/plausible.php
@@ -52,7 +52,10 @@
 						->create( $this )
 						->set_request_url( 'https://plausible.io/api/event' )
 						->set_args(array(
-							'headers'		=> array('Content-Type' => 'application/json; charset=utf-8'),
+							'headers'		=> array(
+								'Content-Type' => 'application/json; charset=utf-8',
+								'User-Agent'	=> $_SERVER['HTTP_USER_AGENT']
+							),
 							'data_format'	=> 'body',
 							'method'		=> 'POST',
 							'body'			=> $body
