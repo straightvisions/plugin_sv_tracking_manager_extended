4d8c92d9004a32d57731fb96d7fb7ac7ddc89abe
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-03-17 19:16:55 +0100

    remove legacy files

--- lib/backend/tpl/subpage_common.php
+++
@@ -1,8 +0,0 @@
-<div class="sv_setting_subpage">
-	<h2><?php _e('Usercentrics', 'sv_tracking_manager_extended'); ?></h2>
-    <div class="sv_setting_flex">
-		<?php
-		echo $module->get_setting( 'local_cache' )->form();
-		?>
-    </div>
-</div>
